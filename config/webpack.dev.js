c4fcba8bbafaedae994078ee4ca299a2ac150eeb
jeff <1411008062@qq.com>
2018-07-30 21:29:55 +0800

    webpack css html js images

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,10 +1,14 @@
 const path = require("path");
+const jsPlugin = require("uglifyjs-webpack-plugin"); // js压缩插件
+const htmlPlugin = require("html-webpack-plugin"); // html压缩插件
+const extractTextPlugin = require("extract-text-webpack-plugin") // css分离
+
 module.exports = {
   mode: "development",
   // 入口文件
   entry: {
     main: path.resolve(__dirname, "../src/main.js"),
-    main2: path.resolve(__dirname, "../src/main2.js")
+    // main2: path.resolve(__dirname, "../src/main2.js")
   },
 
   // 出口文件
@@ -13,9 +17,66 @@ module.exports = {
     filename: "[name].js"
   },
   // css, 图片，压缩
-  module: {},
+  module: {
+    rules: [
+      // css loader
+      {
+        test: /\.css$/,
+        use: extractTextPlugin.extract({
+           fallback: "style-loader",
+             use: "css-loader"
+        })
+        // [
+        //   {loader: "style-loader"},
+        //   {loader: "css-loader"}
+        // ]
+      },
+      // img
+      {
+        test: /\.(png|jepg|jpg|gif)/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 500, //是把小于500B的文件打成Base64的格式，写入JS
+              outputPath: 'images/', //打包后的图片放到images文件夹下
+            } 
+          }
+        ]
+      },
+      {
+        test: /\.(htm|html)$/i,
+        use: ['html-withimg-loader']
+      }
+    ]
+  },
   // 用于生产模块和各项功能；
-  plugins: [],
+  plugins: [
+    new jsPlugin(),
+    new htmlPlugin({
+      template: "./src/index.html",
+      filename: 'index.html',
+      minify: { //html文件压缩
+        minimize: true,
+        removeConments: true,
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+        removeAttributeQuotes: true, // 去掉双引号
+        hash: true, // 避免缓存
+      }
+    }),
+     new extractTextPlugin("css/index.css")
+  ],
   // webpack开发服务项
-  devServer: {}
+  devServer: {
+    // 设置基本目录结构
+    contentBase: path.resolve(__dirname, '../dist'),
+    // 服务器ip地址，本地可以设置localhost
+    host: 'localhost',
+    // 服务端压缩是否开启
+    compress: true,
+    // 端口号
+    port: 8808,
+  }
 };
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import css from './assest/css/index.css'
+
+console.log(1234)
+
+document.getElementById("app").innerHTML = "hello webpack"
